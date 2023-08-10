df212e73dabeaf789a142e685ee2a3da00fad3db
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-08-10 10:20:05 -0700

    updating task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
-* A function that returns prints a series of string using the puts function
-*
-* Return: Always 0 (Success)
-*/
+ * A function that returns prints a series of string using the puts function
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 
