f747ed10841436814527f62bd5e5ae91dcb4e4d2
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-08-10 10:07:34 -0700

    updating task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/*
+* A function that returns prints a series of string using the puts function
+*/
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle"\n);
+	return(0);
+}
