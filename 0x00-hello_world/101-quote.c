f6c554c6d419eab9a8867cc45322c26d91a9b92b
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-08-10 20:20:57 -0700

    updating adv task 2 atmp 16 hot fix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Description: 'and that piece of art is useful - Dora korpar, 2015-10-19'
+ * Return: Always 1 (Error)
+ */
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
