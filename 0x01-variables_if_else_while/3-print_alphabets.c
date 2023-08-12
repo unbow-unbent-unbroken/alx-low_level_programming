f2334a1caae8cd77483f16621bee7d55d0935b6c
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-08-11 20:24:35 -0700

    updating task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints alphabet in lowercase then in uppercase
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
+	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+
+	putchar('\n');
+	return (0);
+}
