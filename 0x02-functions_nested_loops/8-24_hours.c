cb450e14184c389a0afb2a3d45ca7321ea5ea1ad
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-08-16 15:29:58 -0700

    updating task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * jack_bauer - A function that prints every minutes of the day.
+ * starting from 00:00 to 23:59
+ *
+ * @void: int to add
+ *
+ * Return: 0 (Success)
+ *
+ */
+
+void jack_bauer(void)
+{
+	int hr;
+	int min;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min = 60; min++)
+		{
+			_putchar((hr / 10) + '0');
+			_putchar((hr % 10) + '0');
+			_putchar(':');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
