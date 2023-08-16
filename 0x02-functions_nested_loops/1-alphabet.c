37c282b051fcac2eeb09349a8003133af4ee76ad
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-08-16 00:15:16 -0700

    updating task 1 atmp 19

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,8 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - Prints the alphabet in lowercase
  *
- * Description: A program that prints the alphabets in lowercase
  * Return: 0 (Success)
  */
 
