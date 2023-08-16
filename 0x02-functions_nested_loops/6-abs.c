c0b6379c4cda479430325aaeec4ad98068504650
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-08-16 12:09:02 -0700

    updating task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - computes absolute numbers
+ *
+ * Return: 0 (Success)
+ */
+
+int _abs(int)
+{
+	if (j < 0)
+	{
+		return (-j);
+	}
+	else
+	{
+		return (j);
+	}
+}
