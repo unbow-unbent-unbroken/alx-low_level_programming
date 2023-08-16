8edc2b739ee5957083794d633c84e88f8fadba37
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-08-16 11:45:49 -0700

    updating task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,17 +14,21 @@ int print_sign(int n)
 {
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+
 	if (n >= 1)
 	{
 		_putchar('+');
+		return (1);
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
+		return (0);
 	}
 	else
 	{
 		_putchar('-');
+		return (-1);
 	}
 	return (0);
 }
