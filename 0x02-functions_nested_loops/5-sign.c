050923449f28d3111e400d1dc346111d5459f8fd
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-08-16 11:40:06 -0700

    updating task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - return 1 and print + if > zero
+ * return 0 and print 0 if n = zero
+ * return -1 and print - if n < zero
+ *
+ * @n: an int value
+ * Return: 0 (Success)
+ */
+
+int print_sign(int n)
+{
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n >= 1)
+	{
+		_putchar('+');
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+	}
+	else
+	{
+		_putchar('-');
+	}
+	return (0);
+}
