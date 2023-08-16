9d23488441cc67d01ba06a81e88c09a369c285aa
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-08-15 23:29:17 -0700

    updatint tasks

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints the alphabets in lowercase 
+ * Return 0 (Success)
+ */
+
+void print_alpahbet(void)
+{
+	char x;
+
+	for (x = 'a', x <= 'z', x++)
+	{
+		_putchar(x);
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
