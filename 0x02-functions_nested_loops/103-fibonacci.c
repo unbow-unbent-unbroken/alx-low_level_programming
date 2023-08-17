be401ccac2db2ae08028b604360861cb753ac1b7
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-08-16 23:47:53 -0700

    updating adv task 4 no 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that finds and prints the sum of even-valued terms.
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 1;
+	int b = 2;
+	int c = 0;
+	int d;
+
+	while (b < 4000000)
+	{
+		if (b % 2 == 0)
+			c += b;
+
+		d = c;
+		b += a;
+		a = d;
+	}
+	printf("%d\n", c);
+	return (0);
+}
