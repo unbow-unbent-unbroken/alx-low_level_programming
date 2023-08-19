5f3e00278cf33366649f048cc4e46eda67770680
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-08-18 21:19:34 -0700

    updating task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: A program that prints the numbers from 1 to 100
+ * But prints Fizz for mulitiples of 3
+ * prints Buzz for multiples of 5
+ * print FizzBuzz for mulitiples of 3 and 5
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 1; c <= 100; c++)
+	{
+		if (c % 3 == 0 && c % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (c % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (c % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", c);
+		}
+	}
+
+	printf("\n");
+
+	return (0);
+}
