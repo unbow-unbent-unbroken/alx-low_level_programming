7c684e10f5180ad0383c7fc522b77804b4e8d8bb
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-08-18 21:37:55 -0700

    updating task 9

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle
+ * @size: Size of the triangle
+ *
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			_putchar(' ');
+		}
+		for (k = 1; k <= size; k++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -35,7 +35,7 @@ int main(void)
 		}
 	}
 
-	printf("\n");
+	printf("Buzz\n");
 
 	return (0);
 }
