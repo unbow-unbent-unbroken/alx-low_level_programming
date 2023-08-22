b14cc52b90bbbd7e72670f355bf37f1ebcbc9220
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-08-22 16:54:32 -0700

    updating task 6 hot fix

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -3,7 +3,7 @@
 /**
  * puts2 - print one char out of 2 of a string
  * @str: char array string type
- * Description: Print 1st char, then 3rd, then 5th, etc..
+ * Description: Print 2nd char, then 4th, then 6th, etc..
  */
 
 void puts2(char *str)
