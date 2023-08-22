3d161fe5d8ff0767d44598ec2b1bec3625e2c678
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-08-22 14:12:35 -0700

    update task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - A function that prints a string
+ * @str: The string variable of char type
+ *
+ * Return: 0 (Success)
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++
+	}
+	_putchar('\n');
+}
