12d7ad1ea737bcba2a258b01d5fb140f3dc6be31
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-09-07 07:12:07 -0700

    updating adv task 1 hot fix

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,62 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
- * argstostr - Concatenates all the arguments of a program.
- * @ac: The number of arguments.
- * @av: An array of argument strings.
- *
- * Return: A pointer to the concatenated string or NULL on failure.
+ * argstostr - concatenate all arguments of your program with newline
+ * @ac: argument count
+ * @av: double pointer to array of strings passed to main
+ * Return: Null if fail, else return pointer to new string
  */
 
 char *argstostr(int ac, char **av)
 {
-	char *concatenated;
-	int total_length = 0;
-	int i, j;
-	int index = 0;
+	char *a, *retp;
+	int i, j, total;
 
-	/* Check for invalid input */
 	if (ac == 0 || av == NULL)
 		return (NULL);
 
-	/* Calculate the total length of the concatenated string */
-	for (i = 0; i < ac; i++)
+	for (i = 0, total = 0; i < ac; i++)
 	{
-		int arg_length = 0;
-
-		while (av[i][arg_length] != '\0')
-		{
-			arg_length++;
-			total_length++;
-		}
-
-		/* Add 1 for the newline character */
-		total_length++;
+		for (j = 0; *(*(av + i) + j) != '\0'; j++, total++)
+			;
+		total++;
 	}
+	total++;
 
-	/* Allocate memory for the concatenated string */
-	concatenated = (char *)malloc((total_length + 1) * sizeof(char));
+	a = malloc(total * sizeof(char));
+	if (a == NULL)
+		return (NULL);
 
-	if (concatenated == NULL)
-		return (NULL); /* Memory allocation failed */
-
-	/* Copy each argument and add a newline character */
+	retp = a;
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
 		{
-			concatenated[index] = av[i][j];
-			index++;
+			*a = av[i][j];
+			a++;
 		}
-		/* Add a newline character */
-		concatenated[index] = '\n';
-		index++;
+		*a = '\n';
+		a++;
 	}
 
-	/* Null-terminate the concatenated string */
-	concatenated[index] = '\0';
-
-	return (concatenated);
+	return (retp);
 }
