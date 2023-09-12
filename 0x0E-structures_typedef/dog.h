7f08ec10f46d813721f18e0a1e0a979830aeaca7
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-09-12 04:01:20 -0700

    updating task 0 betty style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Defines a dog with name, age, and owner.
+ * @name: Pointer to a character representing the dog's name.
+ * @age: Floating-point number representing the dog's age.
+ * @owner: Pointer to a character representing the dog's owner.
+ *
+ * Description: This structure defines a dog with its name, age, and owner.
+ */
+
 struct dog {
 	char *name;
 	float age;
