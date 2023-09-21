e0ddd2dfc62eddffc62d7135de3677eaf8479ef6
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-09-21 11:52:47 -0700

    updating task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * print_list - A function that prints all the elements of a list
+ * @h: The pointer to the nodes address
+ *
+ * Return: The number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t node_count = 0;
+
+	for (; h != NULL; h = h->next)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		node_count++;
+	}
+	return (node_count);
+}
