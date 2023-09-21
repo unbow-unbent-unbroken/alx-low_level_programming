ed2abf780f2bf661bf3db77a24ee6797da906150
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-09-21 14:57:40 -0700

    updating adv task 1

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * first - function executed before main
+ * Return: no return.
+ */
+
+void __attribute__((constructor)) first_function(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -28,4 +28,6 @@ list_t *add_node_end(list_t **head, const char *str);
 
 void free_list(list_t *head);
 
+void __attribute__((constructor)) first_function(void);
+
 #endif /* LISTS_H */
