ab4f62eb088949847e3d0ee555f7f33cf9b0955a
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-09-21 15:02:15 -0700

    making betty happy

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,11 +2,13 @@
 #include "lists.h"
 
 /**
- * first - function executed before main
+ * first_function - Function executed before main
+ *
  * Return: no return.
  */
 
 void __attribute__((constructor)) first_function(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
