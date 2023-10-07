e4ffca514917c5bca874b15f55ed3d3261f1ed9b
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-10-06 17:32:54 -0700

    hot fixing task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,8 +1,9 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <fcntl.h>
 #include "main.h"
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdlib.h>
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
@@ -14,45 +15,31 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	if (filename == NULL)
-	{
-		return (0);
-	}
-
-	int file_descriptor;
-	ssize_t bytes_read, bytes_written;
+	int fd;
+	ssize_t lenr, lenw;
 	char *buffer;
 
-	file_descriptor = open(filename, O_RDONLY);
-	if (file_descriptor == -1)
-	{
+	if (filename == NULL)
 		return (0);
-	}
-
-	buffer = malloc(letters);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
-		close(file_descriptor);
+		close(fd);
 		return (0);
 	}
-
-	bytes_read = read(file_descriptor, buffer, letters);
-	if (bytes_read == -1)
+	lenr = read(fd, buffer, letters);
+	close(fd);
+	if (lenr == -1)
 	{
-		close(file_descriptor);
 		free(buffer);
 		return (0);
 	}
-
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-
-	close(file_descriptor);
+	lenw = write(STDOUT_FILENO, buffer, lenr);
 	free(buffer);
-
-	if (bytes_written != bytes_read)
-	{
+	if (lenr != lenw)
 		return (0);
-	}
-
-	return (bytes_written);
+	return (lenw);
 }
