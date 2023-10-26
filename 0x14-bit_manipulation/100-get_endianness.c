217f0074f95a39e1ca7cdd4c6df4ada763db98c0
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-10-26 12:58:35 -0700

    updating task 6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * get_endianness - Checks the endianness of the machine
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *c = (char *)&i;
+
+	if (*c)
+	{
+		return (1); /* Little endian */
+	}
+	else
+		return (0); /* Big endian */
+}
