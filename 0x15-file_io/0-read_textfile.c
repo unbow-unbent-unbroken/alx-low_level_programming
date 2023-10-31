8c7210a07b88b27af9aaa3f027ccdb2fb13278de
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-10-30 19:09:38 -0700

    updating task 1

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints it to the POSIX standard output
+ * @filename: the name of the file to read
+ * @letters: the maximum number of letters to read and print
+ *
+ * Return: the actual number of letters read and printed, or 0 on failure
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd, n_read, n_written;
+	char *buf;
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters + 1));
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	n_read = read(fd, buf, letters);
+	if (n_read == -1)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	n_written = write(STDOUT_FILENO, buf, n_read);
+	if (n_written == -1 || n_written != n_read)
+	{
+		free(buf);
+		close(fd);
+		return (0);
+	}
+
+	free(buf);
+	close(fd);
+	return (n_written);
+}
