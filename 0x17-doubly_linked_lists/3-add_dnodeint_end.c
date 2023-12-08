dddf702221ff351636443e0e0f18ce0b59c93458
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-12-08 08:28:18 -0800

    hot fix task 3

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,8 +1,7 @@
 #include "lists.h"
-  
+
 /**
- * add_dnodeint_end - adds a new node at the end
- * of a dlistint_t list
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list
  *
  * @head: head of the list
  * @n: value of the element
@@ -11,22 +10,24 @@
 
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-        dlistint_t *new_node;
-        dlistint_t *h;
+	dlistint_t *h;
+	dlistint_t *new_node;
 
-        new_node = malloc(sizeof(dlistint_t));
-        if (new_node == NULL)
-                return (NULL);
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
 
-        new_node->n = n;
-        new_node->next = NULL;
-        h = *head;
+	new_node->n = n;
+	new_node->next = NULL;
 
-        if (h != NULL)
-        {
-                while (h->next != NULL)
-                        h = h->next;
-        }
+	h = *head;
+
+	if (h != NULL)
+	{
+		while (h->next != NULL)
+			h = h->next;
+		h->next = new_node;
+	}
 	else
 	{
 		*head = new_node;
@@ -34,5 +35,5 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 
 	new_node->prev = h;
 
-        return (new_node);
+	return (new_node);
 }
