ce5aae7f3af94ea65040164d65509ac4855545af
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-12-08 18:39:52 -0800

    task 5 updating file

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Function that returns the nth node of a slistint_t linked list
+ * @head: Pointer to the head of the node
+ * @index: Is the index of the node starting from 0
+ *
+ * Return: NULL is node does not exist
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int i;
+	dlistint_t *current = head;
+
+	for (i = 0; current != NULL && i < index; i++)
+	{
+		current = current->next;
+	}
+
+	return (current);
+}
