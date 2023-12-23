77e32e8b89a02cb0239d468862995eb5a449ff75
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-12-22 17:41:20 -0800

    hot fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -24,10 +24,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->array = malloc(sizeof(hash_table_t *) * size);
 
 	if (new_table->array == NULL)
-	{
-		free(new_table);
 		return (NULL);
-	}
 
 	/*Initialize each element of the array to NULL*/
 	for (i = 0; i < size; i++)
