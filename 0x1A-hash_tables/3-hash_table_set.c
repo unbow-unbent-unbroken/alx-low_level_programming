aca4ef5f2aa6b5e32780b09e1fbebeb074d5ab8d
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-12-22 18:50:27 -0800

    updating task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,79 @@
+#include "hash_tables.h"
+
+/**
+ * create_node - Creates a new hash node.
+ * @key: The key (string) for the new node.
+ * @value: The value (string) for the new node.
+ *
+ * Return: A pointer to the new node, or NULL on failure.
+ */
+
+static hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->key = strdup(key);
+	if (new_node->key == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->value = strdup(value);
+	if (new_node->value == NULL)
+	{
+		free(new_node->key);
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = NULL;
+
+	return (new_node);
+}
+
+/**
+ * hash_table_set - Adds an element to the hash table.
+ * @ht: The hash table to add or update the key/value pair.
+ * @key: The key (string) to add.
+ * @value: The value associated with the key (string). Must be duplicated.
+ *
+ * Return: 1 if it succeeded, 0 otherwise.
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_node, *temp_node;
+	unsigned long int index;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+
+	index = key_index((unsigned char *)key, ht->size);
+
+	temp_node = ht->array[index];
+	while (temp_node != NULL)
+	{
+		if (strcmp(temp_node->key, key) == 0)
+		{
+			free(temp_node->value);
+			temp_node->value = strdup(value);
+			if (temp_node->value == NULL)
+				return (0);
+			return (1);
+		}
+		temp_node = temp_node->next;
+	}
+
+	new_node = create_node(key, value);
+	if (new_node == NULL)
+		return (0);
+
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+
+	return (1);
+}
