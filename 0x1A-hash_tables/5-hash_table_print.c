c40cb30b2e28e6bd6135acefb9b7565b4ca54e06
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-12-22 19:30:42 -0800

    updating task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -4,9 +4,29 @@
  * hash_table-print - A function that prints a hash table
  * @ht: Is the hash table
  *
- * Return: print nothing if ht is NULL.
  */
 
 void hash_table_print(const hash_table_t *ht)
 {
+	hash_node_t *node;
+	unsigned long int i;
+	int first = 1;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (!first)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			first = 0;
+			node = node->next;
+		}
+	}
+	printf("}\n")
 }
