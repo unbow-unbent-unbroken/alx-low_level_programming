413357bf7eb380b8773aa06e7c36a76874213818
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-12-22 17:40:15 -0800

    updated fix

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -15,11 +15,14 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	/*Allocate memory for the hash table structure*/
 	new_table = malloc(sizeof(hash_table_t));
+
 	if (new_table == NULL)
 		return (NULL);
 
 	/*Allocate memory or the array f pointers to linked lists*/
+	new_table->size = size;
 	new_table->array = malloc(sizeof(hash_table_t *) * size);
+
 	if (new_table->array == NULL)
 	{
 		free(new_table);
