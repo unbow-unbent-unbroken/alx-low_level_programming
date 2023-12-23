c506fb409881962077c400353ec77a47d9baa1ae
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2023-12-22 20:30:13 -0800

    hot fix for task 5&6

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,16 +1,17 @@
-#include "hash_tables.h"
+#include "./hash_tables.h"
 
 /**
- * hash_table_print - A function that prints a hash table
- * @ht: Is the hash table
+ * hash_table_print - Prints a hash table.
+ * @ht: A pointer to the hash table to print.
  *
+ * Description: Key/value pairs are printed in the order
+ *              they appear in the array of the hash table.
  */
-
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *node;
 	unsigned long int i;
-	int first = 1;
+	unsigned char comma_flag = 0;
 
 	if (ht == NULL)
 		return;
@@ -18,15 +19,21 @@ void hash_table_print(const hash_table_t *ht)
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
-		node = ht->array[i];
-		while (node != NULL)
+		if (ht->array[i] != NULL)
 		{
-			if (!first)
+			if (comma_flag == 1)
 				printf(", ");
-			printf("'%s': '%s'", node->key, node->value);
-			first = 0;
-			node = node->next;
+
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				printf("'%s': '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+			}
+			comma_flag = 1;
 		}
 	}
-	printf("}\n")
+	printf("}\n");
 }
--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,32 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_delete - A functio that deletes a hash table
- * @ht: Is the hash table
- *
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
  */
-
 void hash_table_delete(hash_table_t *ht)
 {
-	hash_node_t *node, *temp_node;
+	hash_table_t *head = ht;
+	hash_node_t *node, *tmp;
 	unsigned long int i;
-
-	if (ht == NULL)
-		return;
 
 	for (i = 0; i < ht->size; i++)
 	{
-		node = ht->array[i];
-		while (node != NULL)
+		if (ht->array[i] != NULL)
 		{
-			temp_node = node->next;
-			free(node->key);
-			free(node->value);
-			free(node);
-			node = temp_node;
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				tmp = node->next;
+				free(node->key);
+				free(node->value);
+				free(node);
+				node = tmp;
+			}
 		}
 	}
-
-	free(ht->array);
-	free(ht);
+	free(head->array);
+	free(head);
 }
