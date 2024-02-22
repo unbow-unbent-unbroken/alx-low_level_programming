29f5e1e3755013400a328cd39222ae3956028b08
unbow-unbent-unbroken <iyoboiyoha@outlook.com>
2024-02-22 20:45:28 +0100

    anoda fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """Module that calculates the perimeter of an island in a grid."""
 
+
 def num_water_neighbors(grid, i, j):
     """Returns the number of water neighbors a cell has in a grid."""
 
@@ -17,8 +18,10 @@ def num_water_neighbors(grid, i, j):
 
     return num
 
+
 def island_perimeter(grid):
     """Returns the perimeter of the island in grid."""
+
     perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
